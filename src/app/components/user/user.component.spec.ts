2878fbf23157b2a6b46037ea62c0d5b5bd70492b
Patil <manish.a.patil@capgemini.com>
2019-02-19 16:55:15 +0530

    Update the spec for User component

--- src/app/components/user/user.component.spec.ts
+++ src/app/components/user/user.component.spec.ts
@@ -1,6 +1,10 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { of } from 'rxjs';
+
 import { UserComponent } from './user.component';
+import { DataService } from './../../services/data.service';
 
 describe('UserComponent', () => {
   let component: UserComponent;
@@ -9,15 +13,16 @@ describe('UserComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      declarations: [UserComponent]
+      declarations: [UserComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
-      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UserComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();      
+
+    fixture.detectChanges();
   });
 
   it('should create the Component', () => {
@@ -28,3 +33,77 @@ describe('UserComponent', () => {
     expect(compiled.querySelector("h1").textContent).toContain("Users");
   });
 });
+
+const dataServiceStub = {
+  getUsers() {
+    const users = [
+      {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+          "street": "Kulas Light",
+          "suite": "Apt. 556",
+          "city": "Gwenborough",
+          "zipcode": "92998-3874",
+          "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+          }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+          "name": "Romaguera-Crona",
+          "catchPhrase": "Multi-layered client-server neural-net",
+          "bs": "harness real-time e-markets"
+        }
+      }];
+
+    return of(users);
+  },
+  getPosts() {
+    const posts = [
+      {
+        "userId": 1,
+        "id": 1,
+        "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+      },
+      {
+        "userId": 1,
+        "id": 2,
+        "title": "qui est esse",
+        "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+      }]
+
+    return of(posts);
+  }
+};
+
+describe('User Component', () => {
+  let component: UserComponent;
+  let fixture: ComponentFixture<UserComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      declarations: [UserComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [{ provide: DataService, useValue: dataServiceStub }]
+    })
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should get fake data and render the component', () => {
+    fixture.detectChanges();
+
+    const compiled = fixture.debugElement.nativeElement;    
+    expect(compiled.querySelector("h5").textContent).toContain("Leanne Graham");
+  });
+})
